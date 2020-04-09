618b2871f3e7cce8d1663a772774121ee5d8d01e
Joe Garibaldi <joe@Joes-MBP.lan>
2020-04-08 18:28:26 -0700

    worked on string manipulation--still have a lot to do

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,13 +32,28 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if(s1.length()>s2.length()) {
+			return s1;
+		}else {
+			return s2;
+		}
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+		
+		if(s.contains("underscores")==true) {
+			for (int i = 0; i < s.length(); i++) {
+				if(s.charAt(i)==' ') {
+					String t1=s.substring(0, i-1);
+					String t2=s.substring(i+1, s.length()-1);
+			s=t1+"_"+t2;
+				
+				}
+			}
+		}
+	return s;
 	}
 
 	
@@ -46,35 +61,110 @@ public class StringMethods {
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
+		int divider = 0;
+		String name1;
+		String name2;
+		String name3;
+		
+		for (int i = 0; i < s1.length(); i++) {
+			if(s1.charAt(i)==' ') {
+				if(i != 0 ) {
+					if(i !=s1.length()-1) {
+						if(s1.charAt(i+1) != ' ') {
+							divider=i;
+						}
+					}
+				}
+			}
+		}
+		name1=s1.substring(divider);
+		
+		for (int i = 0; i < s2.length(); i++) {
+			if(s2.charAt(i)==' ') {
+				if(i != 0 ) {
+					if(i !=s2.length()-1) {
+						if(s2.charAt(i+1) != ' ') {
+							divider=i;
+						}
+					}
+				}
+			}
+		}
+		name2=s2.substring(divider);
+		
+		for (int i = 0; i < s3.length(); i++) {
+			if(s3.charAt(i)==' ') {
+				if(i != 0 ) {
+					if(i !=s3.length()-1) {
+						if(s3.charAt(i+1) != ' ') {
+							divider=i;
+						}
+					}
+				}
+			}
+		}
+		name3=s3.substring(divider);
+		
+		if(name1.compareTo(name2)<0 && name1.compareTo(name3)<0) {
+			return s1;
+		} else if(name2.compareTo(name3)<0 && name2.compareTo(name1)<0) {
+			return s2;
+		} else if(name3.compareTo(name1)<0 && name3.compareTo(name2)<0) {
+			return s1;
+		} 
+		
 		return null;
 	}
 	
 	
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int yeehaw=0;
+		for (int i = 0; i < s.length(); i++) {
+			yeehaw+=Character.getNumericValue(s.charAt(i));
+		}
+		return yeehaw;
 	}
 	
 	
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int yeehaw=0;
+		//sorry, but I keep thinking of a line from a 10-minute western movie my friend made
+		// at the end the hero says "looks like you just yeed your last haw, partner."
+		for (int i = 0; i < s.length(); i++) {
+			if(s.substring(i).equals(substring)) {
+				yeehaw++;
+			}
+		}
+		return yeehaw;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+		byte[] b=s.getBytes();
+		
+		return Utilities.encrypt(b, (byte) key);
+		
 	}
 
 	// Call Utilities.decrypt to decrypt the cyphertext
 	public static String decrypt(String s, char key) {
-		return null;
+		return Utilities.decrypt(s, (byte) key);
 	}
 
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
+		int yeehaw=0;
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i)==' ') {
+			   if(s.substring(i-substring.length(), i).equals(substring)) {
+				   yeehaw++;
+			   }
+			}
+		}
 		return 0;
 	}
 	
@@ -91,6 +181,12 @@ public class StringMethods {
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
+		String uncluttered=new String();
+		for (int i = 0; i < s.length(); i++) {
+		if(Character.isLetter(s.charAt(i))==true) {
+			uncluttered=uncluttered+s.charAt(i);
+		}
+		}
 		return true;
 	}
 	
